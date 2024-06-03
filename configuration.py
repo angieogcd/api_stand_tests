a0081fea0c03eb39d1a6644867df834a1fccddea
Ruth Ordoñez <ruth.ordg@gmail.com>
2024-06-03 00:52:12 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,16 @@
+#URL base del servicio de Urban Grocers
+URL_SERVICE ="https://cnt-0a55a091-c6f1-42db-af25-9dcdbbb88557.containerhub.tripleten-services.com"
+
+# Agregar docs
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH ="/api/db/resources/user_model.csv"
+
+#Creacion de un usuario
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+#Variable de ruta para solicitud POST para buscar kitsXproductos:
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,16 @@
+#Almacena info que se envia en solicitud/cuerpo-encabezados
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+
+#solicitud POST-kitsXproductos:
+product_ids = {
+    "ids": [1, 2, 3]
+}
